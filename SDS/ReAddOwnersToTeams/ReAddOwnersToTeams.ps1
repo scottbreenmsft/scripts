6b61f7a83bc198c0211b714b24cf213f0af7b06e
Scott Breen <39719539+scottbreenmsft@users.noreply.github.com>
2020-04-24 13:08:49 +1000

    Update ReAddOwnersToTeams.ps1
    add transcript

--- SDS/ReAddOwnersToTeams/ReAddOwnersToTeams.ps1
+++ SDS/ReAddOwnersToTeams/ReAddOwnersToTeams.ps1
@@ -1,5 +1,5 @@
-$tempuser="<enter a test user>"
-
+Start-Transcript
+$tempuser="<temp user upn - requried for groups where there is only one owner>"
 Connect-MicrosoftTeams
 Connect-azuread
 
@@ -13,7 +13,7 @@ $teams=$teams | out-gridview -PassThru
 #provide prompt
 write-host "actioning $($teams.count) teams. continue? Enter to continue. Ctrl+C to exit"
 read-host 
-Write-Host "Continuing"
+Write-Host "Continuing and actioning these teams.`n$($teams.displayName -join "`n")"
 
 #monitor the progress
 $progressCount=0
@@ -24,27 +24,27 @@ foreach ($team in $teams) {
     write-host "actioning $($team.displayName)"
     $users=get-teamuser -GroupId $team.objectid | where {$_.user -like "*@*" -and $_.role -eq "owner"}
 
-    write-host "`tcurrent owners $($users.user -join ";")"
+    write-host "     current owners $($users.user -join ";")"
 
     #add temp owner
     If (-not ($users.count -ge 2)) {
         #add temp owner
-        write-host "`tadding temp owner $tempuser to $($team.displayName)"
+        write-host "     adding temp owner $tempuser to $($team.displayName)"
         add-teamuser -GroupId $team.objectid -User $tempuser -role Owner
     }
 
     #remove owner and re-add
     foreach ($user in $users) {
-        write-host "`tremove $($user.user)"
+        write-host "     remove $($user.user)"
         remove-teamuser -GroupId $team.objectid -User $user.user -role Owner
 
-        write-host "`tadd $($user.user)"
+        write-host "     add $($user.user)"
         add-teamuser -GroupId $team.objectid -User $user.user -role Owner
     }
 
     #remove temp owner
     If (-not ($users.count -ge 2)) {
-        write-host "`tremoving temp owner $tempuser from $($team.displayName)"
+        write-host "     removing temp owner $tempuser from $($team.displayName)"
         remove-teamuser -GroupId $team.objectid -User $tempuser -role Owner
     }
 
@@ -52,3 +52,4 @@ foreach ($team in $teams) {
 
 
 
+Stop-Transcript
