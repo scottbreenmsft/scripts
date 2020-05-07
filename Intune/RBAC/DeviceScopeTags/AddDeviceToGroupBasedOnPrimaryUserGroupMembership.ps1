38c97f263283b82e7948bb74426bd9866b80c8d3
Scott Breen <39719539+scottbreenmsft@users.noreply.github.com>
2020-05-08 08:14:12 +1000

    Added paging
    Added paging to Get-Devices and Get-GroupMembers functions

--- Intune/RBAC/DeviceScopeTags/AddDeviceToGroupBasedOnPrimaryUserGroupMembership.ps1
+++ Intune/RBAC/DeviceScopeTags/AddDeviceToGroupBasedOnPrimaryUserGroupMembership.ps1
@@ -11,6 +11,7 @@ Version History
 0.4    13/01/2020    Changed to use app only authentication
 0.5    13/01/2020    Changed output to work with Azure Automation Runbooks
 0.6    7/02/2020     Changed two attributes to be script parameters to work with Azure Automation
+0.7    Scott Breen    8/05/2020     Added paging to Get-Devices and Get-GroupMembers functions
 #>
 
 ####################################################
@@ -223,46 +224,75 @@ Function Get-UserGroups {
 }
 
 ####################################################
+
+####################################################
 Function Get-GroupMembers {
-    
+	
 [cmdletbinding()]
     param (
         $id
     )
 
-    
-    $graphApiVersion = "Beta"
-    $Resource = "groups/$id/transitiveMembers"
+	
+	$graphApiVersion = "Beta"
+	$Resource = "groups/$id/transitiveMembers"
     $body='{"securityEnabledOnly": true}'
-    
-    try
-    {
+	
+	try
+	{
+        $results=@()
+		$uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
+		$result=(Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get)
+        $results+=$result.value.id
 
-        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
-        (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).value.id
+        #page if necessary - https://docs.microsoft.com/en-us/graph/paging
+        if ($result."@odata.nextLink") {
+            write-verbose "$($results.count) returned. More results are available, will begin paging."
+            $noMoreResults=$false
+            do {
 
-    }
-    
-    catch
-    {
-        
-        $ex = $_.Exception
+                #retrieve the next set of results
+                $result=Invoke-RestMethod -Uri $result."@odata.nextLink" -Headers $authToken -Method Get -ErrorAction Continue
+                $results+=$result.value.id
+
+                #check if we need to continue paging
+                If (-not $result."@odata.nextLink") {
+                    $noMoreResults=$true
+                    write-verbose "$($results.count) returned. No more pages."
+                } else {
+                    write-verbose "$($results.count) returned so far. Retrieving next page."
+                }
+            } until ($noMoreResults)
+        }
+
+
+        return $results
+
+	}
+	
+	catch
+	{
+		
+		$ex = $_.Exception
         If ($ex.Response) {
-            $errorResponse = $ex.Response.GetResponseStream()
-            $reader = New-Object System.IO.StreamReader($errorResponse)
-            $reader.BaseStream.Position = 0
-            $reader.DiscardBufferedData()
-            $responseBody = $reader.ReadToEnd();
-            write-verbose "Response content:`n$responseBody" 
+		    $errorResponse = $ex.Response.GetResponseStream()
+		    $reader = New-Object System.IO.StreamReader($errorResponse)
+		    $reader.BaseStream.Position = 0
+		    $reader.DiscardBufferedData()
+		    $responseBody = $reader.ReadToEnd();
+		    write-verbose "Response content:`n$responseBody" 
             Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
         } else {
             write-error $ex.message
         }
-        break
-        
-    }
-    
+		break
+		
+	}
+	
 }
+####################################################
+
+####################################################
 Function Get-User {
     
 [cmdletbinding()]
@@ -302,8 +332,9 @@ Function Get-User {
     }
     
 }
+####################################################
 
-
+####################################################
 Function Get-Devices {
     
 [cmdletbinding()]
@@ -320,7 +351,7 @@ param
     $graphApiVersion = "beta"
     $Resource = "deviceManagement/managedDevices"
 
-    If ($filterByEnrolledWithinMinutes) {
+    If ($filterByEnrolledWithinMinutes -and $filterByEnrolledWithinMinutes -ne 0) {
         $minutesago = "{0:s}" -f (get-date).addminutes(0-$filterByEnrolledWithinMinutes) + "Z"
         $filter = "?`$filter=enrolledDateTime ge $minutesAgo"
     } else {
@@ -334,9 +365,33 @@ param
     
     try
     {
-        
-        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)$filter"
-        Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get
+        $results=@()
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)$($filter)"
+        write-host $uri
+        $result=Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get
+        $results+=$result
+
+        #page if necessary - https://docs.microsoft.com/en-us/graph/paging
+        if ($result."@odata.nextLink") {
+            write-verbose "$($results.count) returned. More results are available, will begin paging."
+            $noMoreResults=$false
+            do {
+
+                #retrieve the next set of results
+                $result=Invoke-RestMethod -Uri $result."@odata.nextLink" -Headers $authToken -Method Get -ErrorAction Continue
+                $results+=$result
+
+                #check if we need to continue paging
+                If (-not $result."@odata.nextLink") {
+                    $noMoreResults=$true
+                    write-verbose "$($results.count) returned. No more pages."
+                } else {
+                    write-verbose "$($results.count) returned so far. Retrieving next page."
+                }
+            } until ($noMoreResults)
+        }
+
+        return $results
 
     }
     
