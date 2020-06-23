e7fc4da40e68fb4aaa9af4816231aa85dc88b773
Scott Breen <39719539+scottbreenmsft@users.noreply.github.com>
2020-06-23 21:26:38 +1000

    Update CustomDock.sh

--- Intune/macOS/CustomDock.sh
+++ Intune/macOS/CustomDock.sh
@@ -5,7 +5,7 @@
 
 #sleep 10
 
-echo dock config: Removing Dock Persistent Apps | tee -a /var/log/install.log
+echo dock config: Removing Dock Persistent Apps | tee -a ~/library/Logs/script-dock.log
 
 
 dockitems=( "/Applications/Microsoft Edge.app"
@@ -15,11 +15,11 @@ dockitems=( "/Applications/Microsoft Edge.app"
             "/Applications/Microsoft Teams.app"
             "/Applications/Microsoft PowerPoint.app"
             "/Applications/Microsoft OneNote.app"
-	          "/Applications/Company Portal.app"
-            "/Applications/App Store.app"
-            "/Applications/System Preferences.app")
+	    "/Applications/Company Portal.app"
+            "/System/Applications/App Store.app"
+            "/System/Applications/System Preferences.app")
 
-echo dock config: Looking for required applications... | tee -a /var/log/install.log
+echo dock config: Looking for required applications... | tee -a ~/library/Logs/script-dock.log
 
 while [[ $ready -ne 1 ]];do
 
@@ -29,7 +29,7 @@ while [[ $ready -ne 1 ]];do
     if [[ -a "$i" ]]; then
       echo "$i found!"
     else
-      echo "dock config: $i not installed yet"  | tee -a /var/log/install.log
+      echo "dock config: $i not installed yet"  | tee -a ~/library/Logs/script-dock.log
       let missingappcount=$missingappcount+1
 
     fi
@@ -39,39 +39,40 @@ while [[ $ready -ne 1 ]];do
 
   if [[ $missingappcount -eq 0 ]]; then
     ready=1
-    echo "dock config: All apps found, lets prep the dock" | tee -a /var/log/install.log
+    echo "dock config: All apps found, lets prep the dock" | tee -a ~/library/Logs/script-dock.log
   else
-    echo "dock config: Waiting for 60 seconds" | tee -a /var/log/install.log
+    echo "dock config: Waiting for 60 seconds" | tee -a ~/library/Logs/script-dock.log
     sleep 60
   fi
 
 
 done
 
-echo dock config: Clearing Dock | tee -a /var/log/install.log
+echo dock config: Clearing Dock | tee -a ~/library/Logs/script-dock.log
 defaults delete ~/Library/Preferences/com.apple.dock persistent-apps
 defaults delete ~/Library/Preferences/com.apple.dock persistent-others
 
 
 for i in $dockitems; do
-  echo Looking for "$i" | tee -a /var/log/install.log
+  echo Looking for "$i" | tee -a ~/library/Logs/script-dock.log
   if [[ -a "$i" ]] ; then
-    echo dock config: Adding $i to Dock | tee -a /var/log/install.log
+    echo dock config: Adding $i to Dock | tee -a ~/library/Logs/script-dock.log
     defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>$i</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
   fi
 done
 
-echo dock config: Enabling Magnification | tee -a /var/log/install.log
+echo dock config: Enabling Magnification | tee -a ~/library/Logs/script-dock.log
 defaults write com.apple.dock magnification -boolean YES
 
-echo dock config: Enable Dim Hidden Apps in Dock | tee -a /var/log/install.log
+echo dock config: Enable Dim Hidden Apps in Dock | tee -a ~/library/Logs/script-dock.log
 defaults write com.apple.dock showhidden -bool true
 
 #echo Enable Auto Hide dock
 #defaults write com.apple.dock autohide -bool true
 
-echo dock config: Enable Minimise Icons into Dock Icons | tee -a /var/log/install.log
+echo dock config: Enable Minimise Icons into Dock Icons | tee -a ~/library/Logs/script-dock.log
 defaults write com.apple.dock minimize-to-application -bool yes
 
-echo dock config: Restarting Dock | tee -a /var/log/install.log
+echo dock config: Restarting Dock | tee -a ~/library/Logs/script-dock.log
 killall Dock
+
