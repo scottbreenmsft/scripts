205dbc75dc2293deceba12f717f316f9b14a0a38
Scott Breen <39719539+scottbreenmsft@users.noreply.github.com>
2020-08-17 16:45:40 +1000

    Update ReinstallmacOS.sh

--- Intune/macOS/ReinstallmacOS.sh
+++ Intune/macOS/ReinstallmacOS.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+set -x
 START='/Applications/Install macOS Catalina.app/Contents/Resources/startosinstall'
 
-echo "Looking for OS Installer"
-if test -f "$START"; then
-    echo "startosinstall exists, let's use it"
-  else
-    echo "Downloading Operating System"
-    sudo /usr/sbin/softwareupdate --fetch-full-installer
-fi
+# Re-install current build of macOS
+#ver=$(sw_vers | grep ProductVersion | cut -d':' -f2 | tr -d ' ')
 
-echo "Beginning New Install"
-sudo '/Applications/Install macOS Catalina.app/Contents/Resources/startosinstall' --eraseinstall --agreetolicense --forcequitapps --newvolumename 'Macintosh HD'
+# Download and install specific version of Catalina
+ver="10.15.6"
+
+echo "Downloading Operating System"
+/usr/sbin/softwareupdate --fetch-full-installer  --full-installer-version $ver
+"$START" --eraseinstall --agreetolicense --forcequitapps --newvolumename 'Macintosh HD'
+ 
