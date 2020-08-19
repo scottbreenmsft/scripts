d76bbb16b1ea2b500e79a4da2f4cd2b1f5b8d2cc
Scott Breen <39719539+scottbreenmsft@users.noreply.github.com>
2020-08-20 08:50:31 +1000

    Update Install-Minecraft-Education-Edition.sh

--- Intune/macOS/Minecraft Education Edition/Install-Minecraft-Education-Edition.sh
+++ Intune/macOS/Minecraft Education Edition/Install-Minecraft-Education-Edition.sh
@@ -15,7 +15,7 @@
 ## (including, without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary
 ## loss) arising out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility
 ## of such damages.
-## Feedback: neiljohn@microsoft.com
+## Feedback: scbree@microsoft.com
 
 # Define variables
 
