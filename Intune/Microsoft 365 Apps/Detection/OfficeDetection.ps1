89327ec7916e913e6531056ca9f3ea424644e78e
Scott Breen <39719539+scottbreenmsft@users.noreply.github.com>
2020-12-08 15:27:51 +1000

    Update OfficeDetection.ps1

--- Intune/Microsoft 365 Apps/Detection/OfficeDetection.ps1
+++ Intune/Microsoft 365 Apps/Detection/OfficeDetection.ps1
@@ -1,10 +1,10 @@
-﻿
 ###########################
 #Script Variables
 ###########################
 #Excluded Apps
 #array of strings. eg. @("groove","teams")
 #list: https://docs.microsoft.com/en-us/deployoffice/office-deployment-tool-configuration-options#id-attribute-part-of-excludeapp-element
+## NOTE This is only checked with the product ID is O365ProPlusRetail
 $ExcludedApps=@("groove","lync","OneDrive","Bing")
 
 #ProductReleaseID of the product you want to check for
@@ -14,10 +14,10 @@ $ProductID="O365ProPlusRetail"
 #Platform
 #The platform you want to be installed. x64 or x86.
 $Platform="x64"
-
 ###########################
 
 #Office key
+$NotDetected=$false
 $RegOffice="HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration"
 $RegKeys = get-itemproperty -Path $RegOffice
 
@@ -25,34 +25,47 @@ $RegKeys = get-itemproperty -Path $RegOffice
 $CurrentPlatform=$RegKeys.Platform
 IF ($Platform -ne $CurrentPlatform) {
     write-host "Platform: '$Platform' does not match '$CurrentPlatform'"
-}
+    $NotDetected=$true
 
 #check installed products
 $CurrentProductReleaseIDs=$RegKeys.ProductReleaseIDs
 If ($CurrentProductReleaseIDs -notlike "*$ProductID*") {
     write-host "ProductReleaseIDs: '$ProductID' not found in '$CurrentProductReleaseIDs'"
+    $NotDetected=$true
 }
 
 #Check Excluded Apps
-$CurrentExcludedApps=$RegKeys."O365ProPlusRetail.ExcludedApps"
-IF ($CurrentExcludedApps) {
-    $CurrentExcludedApps=$CurrentExcludedApps.Split(",")
+IF ($ProductID -eq "O365ProPlusRetail") {
+    $CurrentExcludedApps=$RegKeys."O365ProPlusRetail.ExcludedApps"
+    IF ($CurrentExcludedApps) {
+        $CurrentExcludedApps=$CurrentExcludedApps.Split(",")
 
-    #check if extra apps are excluded
-    Foreach ($ExcludedApp in $CurrentExcludedApps) {
-        If ($ExcludedApps -notcontains $ExcludedApp) {
-            write-host "Excluded Apps - Extra: $excludedapp"
+        #check if extra apps are excluded
+        Foreach ($ExcludedApp in $CurrentExcludedApps) {
+            If ($ExcludedApps -notcontains $ExcludedApp) {
+                write-host "Excluded Apps - Extra: $excludedapp"
+                $NotDetected=$true
+            }
+        }
+    }
+
+    If ($ExcludedApps) {
+        #check that all the required excluded apps ARE excluded
+        Foreach ($ExcludedApp in $ExcludedApps) {
+            If ($CurrentExcludedApps -contains $ExcludedApp) {
+                #write-host "contains $excludedapp"
+            } else {
+                write-host "Excluded Apps - Missing: $excludedapp"
+                $NotDetected=$true
+            }
         }
     }
 }
 
-If ($ExcludedApps) {
-    #check that all the required excluded apps ARE excluded
-    Foreach ($ExcludedApp in $ExcludedApps) {
-        If ($CurrentExcludedApps -contains $ExcludedApp) {
-            #write-host "contains $excludedapp"
-        } else {
-            write-host "Excluded Apps - Missing: $excludedapp"
-        }
-    }
+#check detection
+If ($NotDetected) {
+    exit 1
+} else {
+    Write-host "Detected successfully"
+    exit 0
 }
