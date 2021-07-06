e19d7bb1f27adad3961457e7ceb2033d29f90153
Scott Breen <39719539+scottbreenmsft@users.noreply.github.com>
2021-07-06 12:39:06 +1000

    Update ReinstallmacOS.sh

--- Intune/macOS/ReinstallmacOS.sh
+++ Intune/macOS/ReinstallmacOS.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -x
-START='/Applications/Install macOS Catalina.app/Contents/Resources/startosinstall'
+START='/Applications/Install macOS Big Sur.app/Contents/Resources/startosinstall'
 
 # Re-install current build of macOS
-#ver=$(sw_vers | grep ProductVersion | cut -d':' -f2 | tr -d ' ')
+ver=$(sw_vers | grep ProductVersion | cut -d':' -f2 | tr -d ' ')
 
 # Download and install specific version of Catalina
-ver="10.15.6"
+#ver="10.15.6"
 
 echo "Downloading Operating System"
 /usr/sbin/softwareupdate --fetch-full-installer  --full-installer-version $ver
-#"$START" --eraseinstall --agreetolicense --forcequitapps --newvolumename 'Macintosh HD'
- 
+#"$START" --eraseinstall --agreetolicense --forcequitapps --newvolumename 'Macintosh HD' --nointeraction
