e82dc350c715fcd07316458e930bad1e85d8b1dc
Scott Breen <39719539+scottbreenmsft@users.noreply.github.com>
2021-11-22 09:45:52 +1000

    Update ReinstallmacOS.sh

--- Intune/macOS/ReinstallmacOS.sh
+++ Intune/macOS/ReinstallmacOS.sh
@@ -8,7 +8,8 @@ ver=$(sw_vers | grep ProductVersion | cut -d':' -f2 | tr -d ' ')
 #ver="10.15.6"
 
 echo "Downloading Operating System"
-/usr/sbin/softwareupdate --fetch-full-installer  --full-installer-version $ver
+#/usr/sbin/softwareupdate --fetch-full-installer  --full-installer-version $ver
+/usr/sbin/softwareupdate --fetch-full-installer
 
 echo "installing OS"
-#'/Applications/Install macOS Big Sur.app/Contents/Resources/startosinstall' --eraseinstall --agreetolicense --forcequitapps --newvolumename 'Macintosh HD' --nointeraction
+#'/Applications/Install macOS Monterey.app/Contents/Resources/startosinstall' --eraseinstall --agreetolicense --forcequitapps --newvolumename 'Macintosh HD' --nointeraction --passprompt
