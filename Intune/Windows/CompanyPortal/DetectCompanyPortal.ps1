f5c5191496ed6051d4affaa94667f87e2293d408
Scott Breen <39719539+scottbreenmsft@users.noreply.github.com>
2022-08-31 16:52:28 +1000

    Create DetectCompanyPortal.ps1

---
+++ Intune/Windows/CompanyPortal/DetectCompanyPortal.ps1
@@ -0,0 +1,19 @@
+$packageFamilyName="Microsoft.CompanyPortal_8wekyb3d8bbwe"
+$app=Where-Object {$_.packageFamilyName -eq $packageFamilyName}
+if (-not $app) {
+    write-output "Not installed"
+    exit 1
+} else {
+    write-output "Company Portal Installed - $($app.status)"
+
+    #confirmed required component installed
+    $app2=Get-AppxPackage -Allusers | Where-Object {$_.packageFamilyName -eq "Microsoft.Services.Store.Engagement_8wekyb3d8bbwe"}
+    If ($app2) {
+        write-output "Microsoft.Services.Store.Engagement Installed"
+        exit 0
+    } else {
+        write-output "Microsoft.Services.Store.Engagement Missing"
+        exit 1
+    }
+    exit 0
+}
