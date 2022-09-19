c0487baf05d3d294963326002c907f78d6ce8501
Scott Breen <39719539+scottbreenmsft@users.noreply.github.com>
2022-09-19 17:34:19 +1000

    Update ManagedDeviceCertificateExpiryReport.ps1

--- Intune/Reporting/ManagedDeviceCertificateExpiryReport/ManagedDeviceCertificateExpiryReport.ps1
+++ Intune/Reporting/ManagedDeviceCertificateExpiryReport/ManagedDeviceCertificateExpiryReport.ps1
@@ -21,8 +21,7 @@ $devices=$devices | Select-Object Id, DeviceName, DeviceType, IMEI, UserPrincipa
 
 #Provide Management Certificate status summary
 write-host "There are $(($devices | Where-Object {$_.Expired}).count) devices that have expired management certificates" -ForegroundColor red
-write-host "There are $(($devices | Where-Object {$_.DaysUntilExpiry -lt 30}).count) devices that will expire in less than 30 days"
-write-host "There are $(($devices | Where-Object {$_.DaysUntilExpiry -ge 30 -and $_.DaysUntilExpiry -lt 60}).count) devices that will expire in less than 60 days"
+write-host "There are $(($devices | Where-Object {$_.DaysUntilExpiry -le 45}).count) devices that will expire in less than 45 days"
 
 #export the full device list to $fileName
 $devices | Export-Csv $fileName -NoTypeInformation
